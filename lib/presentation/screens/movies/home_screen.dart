160d059fc97fd98992ca3977548930e2258bb563
InflvxDev <seochoa@unicesar.edu.co>
2023-07-30 15:07:33 -0500

    Implementacion de moviedb datasource configurada

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,17 +1,12 @@
-import 'package:cinemapedia/config/constants/environment.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(Environment.movieDBKey),
-      ),
+    return const Scaffold(
+      body: Placeholder()
     );
   }
 }
