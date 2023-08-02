0ece6edf6e2182da507296bd541e60b60d2d1815
InflvxDev <seochoa@unicesar.edu.co>
2023-08-02 11:38:33 -0500

    navegacion entre peliculas, creacion de rutas segun el idmovie

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,14 +1,20 @@
-import 'package:cinemapedia/presentation/screens/screens.dart';
 import 'package:go_router/go_router.dart';
+import 'package:cinemapedia/presentation/screens/screens.dart';
 
-final appRouter = GoRouter(
-  initialLocation: '/',
-  routes: [
+final appRouter = GoRouter(initialLocation: '/', routes: [
 
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const HomeScreen(),
-    ),
-
-  ]
-);
+  GoRoute(
+    path: '/', 
+    builder: (context, state) => const HomeScreen(), 
+    routes: [
+      GoRoute(
+        path: 'movie/:id',
+        builder: (context, state) {
+          final movieid = state.pathParameters['id'] ?? 'No-id';
+          return MovieScreen(
+            movieId: movieid,
+          );
+        },
+      ),
+  ]),
+]);
